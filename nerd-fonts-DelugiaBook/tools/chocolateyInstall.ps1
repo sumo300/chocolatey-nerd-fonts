5f66acf3fbdad7901f73603f07961b7cade9cc87
Michael Sumerano <msumerano@gmail.com>
2022-05-03 22:58:20 -0400

    Fixing installer for Delugia fonts

--- nerd-fonts-DelugiaBook/tools/chocolateyInstall.ps1
+++ nerd-fonts-DelugiaBook/tools/chocolateyInstall.ps1
@@ -16,11 +16,11 @@ Install-ChocolateyZipPackage @packageArgs
 
 # Install all OpenType Fonts in package
 Push-Location $toolsDir
-$fontList = Get-ChildItem *.otf
+$fontList = Get-ChildItem *.otf -Recurse
 
 # Get list of TrueType fonts instead if OpenType fonts are missing
 if ($fontList.Count -le 0) {
-  $fontList = Get-ChildItem *.ttf
+  $fontList = Get-ChildItem *.ttf -Recurse
 }
 
 # Installs fonts in Paths list and keeps track of the list for uninstall later
