00604f78e04ca68a0c65b713682b6103b053e730
Michael Sumerano <msumerano@gmail.com>
2023-05-30 22:37:51 -0400

    Fixing Gohu in 3.0.0

--- nerd-fonts-Gohu/tools/chocolateyInstall.ps1
+++ nerd-fonts-Gohu/tools/chocolateyInstall.ps1
@@ -16,11 +16,11 @@ Install-ChocolateyZipPackage @packageArgs
 
 # Install all Open Type Fonts in package
 Push-Location $toolsDir
-$fontList = Get-ChildItem "*Windows Compatible*.otf" -Recurse
+$fontList = Get-ChildItem "*.otf" -Recurse
 
 # Get list of TrueType fonts instead if OpenType fonts are missing
 if ($fontList.Count -le 0) {
-  $fontList = Get-ChildItem "*Windows Compatible*.ttf" -Recurse
+  $fontList = Get-ChildItem "*.ttf" -Recurse
 }
 
 # Installs fonts in Paths list and keeps track of the list for uninstall later
