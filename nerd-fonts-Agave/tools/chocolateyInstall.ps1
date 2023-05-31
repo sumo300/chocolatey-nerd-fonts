1c14608d370eb02f20a1c16ef851ad2070924ba1
Michael Sumerano <msumerano@gmail.com>
2023-05-30 22:22:33 -0400

    Re-added missing push-location

--- nerd-fonts-Agave/tools/chocolateyInstall.ps1
+++ nerd-fonts-Agave/tools/chocolateyInstall.ps1
@@ -14,12 +14,15 @@ $packageArgs = @{
 
 Install-ChocolateyZipPackage @packageArgs
 
+# Install all OpenType Fonts in package
+Push-Location $toolsDir
+
 # Get list of all Windows Compatible OpenType Fonts in package
-$fontList = Get-ChildItem -Filter "*Windows Compatible*.otf"
+$fontList = Get-ChildItem "*Windows Compatible*.otf"
 
 if ($fontList.Count -le 0) {
   # Get list of all OpenType Fonts in package
-  $fontList = Get-ChildItem -Filter *.otf
+  $fontList = Get-ChildItem *.otf
 }
 
 # Use the TrueType fonts only if the OpenType files are missing
@@ -27,9 +30,10 @@ if ($fontList.Count -le 0) {
   # Get list of all Windows Compatible TrueType Fonts in package
   $fontList = Get-ChildItem "*Windows Compatible*.ttf"
 }
+
 if ($fontList.Count -le 0) {
   # Get list of all TrueType Fonts in package
-  $fontList = Get-ChildItem -Filter *.ttf
+  $fontList = Get-ChildItem *.ttf
 }
 
 # Installs fonts in Paths list and keeps track of the list for uninstall later
