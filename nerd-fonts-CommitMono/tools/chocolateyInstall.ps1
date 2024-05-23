d0a893c6714cc7e56bcf6e0abc249a30defdcbbb
Michael Sumerano <msumerano@gmail.com>
2024-05-23 00:04:28 -0400

    Added CommitMono

---
+++ nerd-fonts-CommitMono/tools/chocolateyInstall.ps1
@@ -0,0 +1,42 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = $env:ChocolateyPackageName
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  softwareName   = $packageName
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  url            = 'https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/CommitMono.zip'
+  checksumType   = 'sha256'
+  checksum       = '99E3E1B6126191CC0F7E49EDE5DC20B65A6B8CA808DA0139B5669E412632BF8D'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+# Install all OpenType Fonts in package
+Push-Location $toolsDir
+
+# Get list of all Windows Compatible OpenType Fonts in package
+$fontList = Get-ChildItem "*Windows Compatible*.otf"
+
+if ($fontList.Count -le 0) {
+  # Get list of all OpenType Fonts in package
+  $fontList = Get-ChildItem *.otf
+}
+
+# Use the TrueType fonts only if the OpenType files are missing
+if ($fontList.Count -le 0) {
+  # Get list of all Windows Compatible TrueType Fonts in package
+  $fontList = Get-ChildItem "*Windows Compatible*.ttf"
+}
+if ($fontList.Count -le 0) {
+  # Get list of all TrueType Fonts in package
+  $fontList = Get-ChildItem *.ttf
+}
+
+# Installs fonts in Paths list and keeps track of the list for uninstall later
+$installCount = Install-ChocolateyFont -Paths $fontList -Multiple
+Write-Host "$installCount fonts installed"
+
+Pop-Location
